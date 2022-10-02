9198fb084e4e20f2676495343dfc1e097b2ec85a
Devil666face <artem1999k@gmail.com>
2022-10-02 12:33:18 +0200

    work vers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from cgitb import text
 import requests
+import re
 
 session = requests.Session()
 response = session.post('https://netstalkers.com/api/auth/jwt/create/', {
@@ -7,19 +7,30 @@ response = session.post('https://netstalkers.com/api/auth/jwt/create/', {
      'username': 'devil666face',
 })
 cookies = session.cookies
-# print(response.text)
-# get_resp = session.get('https://netstalkers.com/api/patrons/post/29/')
-# ans = get_resp.json()
-# for i in range(10):
-try:
-    url = f'https://netstalkers.com/api/patrons/?page={1}'
-    page_json = session.get(url).json()
-    print(page_json)
-except Exception as ex:
-    print(ex)
 
-# print(ans['video'])
-# video_resp = session.get(ans['video'])
-# print('save')
-# with open("video.mp4", "wb") as code:
-#     code.write(video_resp.content)
+video_dict = dict()
+
+def get_title(name):
+    title = re.sub("[^А-Яа-яA-Za-z0-9\s]", "", name)
+    print(f'{title}')
+    return title
+
+for i in range (1,100):
+    try:
+        url = f'https://netstalkers.com/api/patrons/?page={i}'
+        page_json = session.get(url).json()
+        result = page_json.get('results')
+        for item in result:
+            video_dict[item.get('id')] = get_title(item.get('title','video'))
+    except Exception as ex:
+        break
+
+
+for video_id in video_dict:
+    url = f'https://netstalkers.com/api/stream/{video_id}/'
+    print(f'Скачиваю {url}')
+    
+    video_response = session.get(url)
+    print(video_response)
+    with open(f'{video_dict.get(video_id)}.mp4','wb') as video:
+        video.write(video_response.content)
